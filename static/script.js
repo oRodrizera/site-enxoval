9ba91ca37f4aea950b630915d6837f331224c053
Rodrigo <rodrigonuneslimasantos@gmail.com>
2026-03-22 00:53:21 -0300

    primeiro commit

---
+++ static/script.js
@@ -0,0 +1,16 @@
+ // DATA DO CASAMENTO
+ function atualizarContador() {
+    const dataCasamento = new Date("2027-03-20 00:00:00");
+    const agora = new Date();
+
+    const diferenca = dataCasamento - agora;
+
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferenca / (1000 * 60 * 60)) % 24);
+    const minutos = Math.floor((diferenca / (1000 * 60)) % 60);
+    const segundos = Math.floor((diferenca / 1000) % 60);
+
+    document.getElementById("days").innerText = dias; document.getElementById("hours").innerText = horas; document.getElementById("minutes").innerText = minutos; document.getElementById("seconds").innerText = segundos;
+ }
+
+ setInterval(atualizarContador, 1000);
