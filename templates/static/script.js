2a129bce34b2f3a06ac94956adc6b579e91b423e
Rodrigo <rodrigonuneslimasantos@gmail.com>
2026-03-22 15:10:51 -0300

    ajustando texto mobile

--- templates/static/script.js
+++ templates/static/script.js
@@ -14,14 +14,3 @@
  }
 
  setInterval(atualizarContador, 1000);
-
- function copiarPix() {
-   const chave = document.getElementById("chavePix").innerText;
-
-   navigator.clipboard.writeText(chave).then(() => {
-      document.getElementById("mensagemCopiado").innerText = "Nós te agradecemos ❤️";
- })
- .catch(() => {
-   alert("Não foi possível copiar. Copie manualmente 😊");
- })
-}
